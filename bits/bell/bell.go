de43bc64b1c887f24c9053764373a7afba20b8c9
Ilya Tribusean <slash3b@gmail.com>
2024-06-02 15:54:06 +0300

    bell is done! ended up looking through some of the go submitted solutions, until I saw golang.org/x/term :thumbsup:

--- bits/bell/bell.go
+++ bits/bell/bell.go
@@ -2,24 +2,46 @@ package main
 
 import (
 	"os"
+	"syscall"
 	"time"
+
+	"golang.org/x/term"
 )
 
+/*
+notes:
+tty
+stty
+*/
+
 func main() {
-	b := make([]byte, 1, 1)
+	b := make([]byte, 1)
+
+	oldState, _ := term.MakeRaw(int(os.Stdin.Fd()))
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
 
 	for {
 		os.Stdin.Read(b)
 
-		n := int(b[0]) - 0x30
+		input := int(b[0])
 
-		for i := 0; i < n; i++ {
-			os.Stderr.Write([]byte{0x07})
+		switch syscall.Signal(input) {
+		case syscall.SIGINT, syscall.SIGQUIT:
+			return
+		}
+
+		bps := input - 0x30 // ascii offset.
+		if bps > 9 {
+			continue
+		}
+
+		for i := 0; i < bps; i++ {
+			os.Stdout.Write([]byte{0x07})
 
 			// I guessed that system needs a delay to output
 			// a bleep sound, so I've added it. 300ms seems like enough.
-            // not sure why delay is needed.
-			time.Sleep(time.Millisecond * 300)
+			// not sure why delay is needed.
+			time.Sleep(time.Millisecond * 200)
 		}
 	}
 }
