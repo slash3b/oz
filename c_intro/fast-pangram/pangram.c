5fb05788eb39fec92228e93f4c20487eb9c70b1d
Ilya Tribusean <slash3b@gmail.com>
2024-07-16 15:07:08 +0300

    using bit set, almost there but something is off, sorted pangrams for diffing

--- c_intro/fast-pangram/pangram.c
+++ c_intro/fast-pangram/pangram.c
@@ -3,35 +3,21 @@
 #include <stdlib.h>
 
 bool ispangram(char *s) {
+    int res = 0;
 
-    // save init pointer.
-    char *f = s;
-
-    /*
-        printf("int %d\n", s);
-        printf("dereferenced int %d\n", *s);
-        printf("dereferenced string %s\n", *s);
-        printf("string %s\n", s);
-    */
-
-    printf("\n%s\n", f);
+    char ch = 0;
 
     for (char *p = s; *p > 0 ; p++) {
         // to lowercase
         // inspired by https://stackoverflow.com/a/2661917/3478120
-        //
-        *p =  *p > 0x40 && *p < 0x5b ? *p | 0x20 : *p;
+        ch = *p > 0x40 && *p < 0x5b ? *p | 0x20 : *p;
 
+        if (ch > 0x60 && ch < 0x7b) {
+            res |=  1 << ch - 0x61;
+        }
+  }
 
-
-    }
-
-    // should be equal to 26
-
-    printf("\n%s\n", f);
-
-  // TODO implement this!
-  return false;
+  return res == 0b11111111111111111111111111;
 }
 
 int main() {
@@ -41,10 +27,9 @@ int main() {
 
   while ((read = getline(&line, &len, stdin)) != -1) {
     if (ispangram(line)) {
-      //printf("%s", line);
+      printf("%s", line);
     }
 
-    return 0;
   }
 
   if (ferror(stdin)) {
