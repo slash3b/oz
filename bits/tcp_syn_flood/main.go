b92b8b7f6a2f05a0552feaace60e3de8560061ef
Ilya Tribusean <slash3b@gmail.com>
2024-08-01 18:12:36 +0300

    committing, a bit stuck, have to change code a bit

--- bits/tcp_syn_flood/main.go
+++ bits/tcp_syn_flood/main.go
@@ -106,27 +106,59 @@ func (pkh PacketHeader) Time() time.Time {
 func (pkh PacketHeader) Print() {
 	fmt.Println("packet header:")
 	fmt.Printf("  raw %#v\n", pkh)
+	fmt.Printf("  len bytes %d\n", pkh.Len)
 	fmt.Println("  len == untrunc", pkh.Len == pkh.UntruncLen)
 	fmt.Println("  first packet header time", pkh.Time())
 }
 
 type IPv4Header struct {
-	Version int
-	IHL     int
+	Version  int
+	IHL      int
+	TotalLen int
+	// TTL is in seconds
+	TTL      int
+	Protocol InternetProtocol
+	Src      string
+	Dst      string
 }
 
 func NewIPv4Header(src []byte) IPv4Header {
-	return IPv4Header{
-		Version: int((src[0] & 0xf0) >> 4),
-		IHL:     int(src[0] & 0x0f),
+	h := IPv4Header{
+		Version:  int((src[0] & 0xf0) >> 4),
+		IHL:      int((src[0] & 0x0f) << 2),
+		TotalLen: int(littleEndianToUint16(src[2:4])),
+		TTL:      int(src[8]),
+		Protocol: InternetProtocol(src[9]),
 	}
+
+	h.Src = fmt.Sprintf("%d.%d.%d.%d", src[12], src[13], src[14], src[15])
+	h.Dst = fmt.Sprintf("%d.%d.%d.%d", src[16], src[17], src[18], src[19])
+
+	return h
 }
 
 func (iph IPv4Header) Print() {
 	fmt.Println("ipv4 header")
-	fmt.Println("ip header Version:", int(iph.Version))
-	fmt.Println("ip header IHL:", int(iph.IHL))
+	fmt.Println("  ip header | Version:", iph.Version)
+	fmt.Println("  ip header | IHL:", iph.IHL)
+	fmt.Println("  ip header | total length:", iph.TotalLen)
+	fmt.Println("  ip header | ttl seconds:", iph.TTL)
+	fmt.Println("  ip header | IP number:", iph.Protocol)
+	fmt.Println("  ip header | source:", iph.Src)
+	fmt.Println("  ip header | destination:", iph.Dst)
 }
+
+type InternetProtocol int
+
+const (
+	HOPOPT InternetProtocol = iota
+	ICMP
+	IGMP
+	GGP
+	IPinIP
+	ST
+	TCP
+)
 
 // todo: figure out what % of connections are ACKnowledged?
 func main() {
@@ -173,13 +205,28 @@ func main() {
 			panic(fmt.Sprintf("unexpected protocol type %d, expected 2 (IPv4)", protocolType))
 		}
 
+        // reslice to be used in this iteration
+        s := pcap[curr : curr+ph.Len]
+		curr += ph.Len-4 // minus 4 bytes we processed above, this is messy.
+
 		// IPv4 Header
-		ipheader := NewIPv4Header(pcap[curr : curr+ph.Len])
-		curr += ph.Len
+        ipheader := NewIPv4Header(s[:20])
+
+        // 
+
+        // payload
+        
 
 		ipheader.Print()
 
-		break
+        tcppacket := pcap[curr + 24 : curr+ph.Len]
+        fmt.Printf("%#v\n", tcppacket)
+
+        fmt.Println(littleEndianToUint16(tcppacket[:2]))
+        fmt.Println(littleEndianToUint16(tcppacket[2:4]))
+
+        break
+
 
 		c++
 	}
@@ -201,6 +248,16 @@ func littleEndianToUint16(src []byte) uint16 {
 	var res uint16
 
 	for i := 0; i < len(src); i++ {
+		res += uint16(src[i]) << (8 * i)
+	}
+
+	return res
+}
+
+func bigEndianToUint16(src []byte) uint16 {
+	var res uint16
+
+	for i := len(src)-1; i >= 0; i-- {
 		res += uint16(src[i]) << (8 * i)
 	}
 
