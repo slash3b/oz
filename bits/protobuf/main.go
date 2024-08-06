128ea03d971020567f5c26bc8fc7cbfbf61bfa5e
Ilya Tribusean <slash3b@gmail.com>
2024-08-06 21:04:02 +0300

    changed to encode decode a sequence of numbers

--- bits/protobuf/main.go
+++ bits/protobuf/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
-	"strings"
 )
 
 func main() {
@@ -13,78 +13,103 @@ func main() {
 		return
 	}
 
-	a, _ := strconv.ParseUint(args[1], 10, 64)
+	a := uintsfromargs(args)
+
 	fmt.Println("incoming", a)
-    prettyBytes("in bytes (BE):", a)
-    fmt.Println("------------------------------")
+	fmt.Println("------------------------------")
 
 	res := encode(a)
 
-	fmt.Printf("encoded (LE):\t%#x\n\t%08[1]b\n", res)
+	fmt.Printf("varint encoded:\t%#x\n", res)
 
-	res2 := decode(res)
+	b := decode(res)
 
-	fmt.Printf("decoded (BE):\t%#x\n", res2)
-    prettyBytes("\t", res2)
+	fmt.Printf("decoded:\t%#x\n", b)
 
-	return
+	fmt.Println("------------------------------")
+	fmt.Println("outgoing", b)
 
-	_, err := os.Stdout.Write(res)
-
-	if err != nil {
-		panic(err)
+	if !slices.Equal(a, b) {
+		os.Exit(1)
 	}
 }
 
 // encode in little endian byte order
-func encode(src uint64) []byte {
-	if src == 0 {
+func encode(src []uint) []byte {
+	if len(src) == 0 {
 		return nil
 	}
 
 	var res []byte
 
-	for src > 0 {
-		r := src & 0x7f // 0x7f a.k.a. 0b0111_1111, how src % 128 also does the same is beyond me.
+	for _, v := range src {
+		for v > 0 {
+			r := v & 0x7f // 0x7f a.k.a. 0b0111_1111, how src % 128 also does the same is beyond me.
 
-		src = src >> 7
+			v = v >> 7
 
-        // Each byte in the varint has a continuation bit that indicates if the byte that follows it is part of the varint.
-        // see: https://protobuf.dev/programming-guides/encoding/#varints
-        // note: 
-        // a sequence of continuation bits that denotes a group might look like 1, 1, 1, 0,
-        // where last 0 tells this is the end of a sequence.
-        // a single byte group has no continuation, so continueation bit sequence might look liek 1, 1, 1, 0, 0 
-        // where last byte with `0` continuation bit is a standalone int.
-        if src > 0 {
-            r |= 0x80 // 0x80 a.k.a 0b1000_0000
-        }
+			// Each byte in the varint has a continuation bit that indicates if the byte that follows it is part of the varint.
+			// see: https://protobuf.dev/programming-guides/encoding/#varints
+			// note:
+			// a sequence of continuation bits that denotes a group might look like 1, 1, 1, 0,
+			// where last 0 tells this is the end of a sequence.
+			// a single byte group has no continuation, so continueation bit sequence might look liek 1, 1, 1, 0, 0
+			// where last byte with `0` continuation bit is a standalone int.
+			if v > 0 {
+				r |= 0x80 // 0x80 a.k.a 0b1000_0000
+			}
 
-		res = append(res, byte(r))
+			res = append(res, byte(r))
+		}
 	}
 
 	return res
 }
 
+func decode(src []byte) []uint {
+	var res []uint
 
-func decode(src []byte) uint {
-	var res uint
+	a := uint(0)
+	counter := 0 // byte shift counter
 
-	for i := len(src) - 2; i >= 0; i-- {
-		res |= uint(src[i]&0x7f) << uint(7*i)
+	for i := 0; i < len(src); {
+		if src[i]&0x80 == 0 {
+			res = append(res, uint(src[i]&0x7f))
+
+			i++
+
+			continue
+		}
+
+		a |= uint(src[i]&0x7f) << uint(7*counter)
+		counter++
+		i++
+
+		// rely on fact that termination bit should be set to 0
+		if src[i]&0x80 == 0 {
+			a |= uint(src[i]&0x7f) << uint(7*counter)
+
+			res = append(res, a)
+
+			// reset
+			a = 0
+			counter = 0
+
+			i++
+		}
 	}
 
 	return res
 }
 
-func prettyBytes(m string, src any) {
-	r := fmt.Sprintf("%064b", src)
+func uintsfromargs(args []string) []uint {
+	res := make([]uint, len(args[1:]))
 
-	res := []string{}
+	for i, v := range args[1:] {
+		a, _ := strconv.ParseUint(v, 10, 64)
 
-	for i := 0; i < len(r); i += 8 {
-		res = append(res, string(r[i:i+8]))
+		res[i] = uint(a)
 	}
 
-	fmt.Println(m, strings.Join(res, " "))
+	return res
 }
