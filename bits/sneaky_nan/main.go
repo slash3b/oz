74b638b09727cef533595db702be34718f2dbe07
Ilya Tribusean <slash3b@gmail.com>
2024-08-08 17:44:11 +0300

    some formatting

--- bits/sneaky_nan/main.go
+++ bits/sneaky_nan/main.go
@@ -2,63 +2,49 @@ package main
 
 import (
 	"fmt"
-    "math"
+	"math"
 )
 
 func main() {
+	f := encode("bye")
+	fmt.Println("encoded:", f)
 
-	// encode("hello")
-	encode("bye")
+	res := decode(f)
+	fmt.Println("decoded:", res)
 }
 
 func encode(s string) float64 {
-
-	fmt.Printf("input %s %#x\n", s, []byte(s))
-
 	if len(s) > 6 {
 		return 0
 	}
 
-	var c uint
+	var c uint64
 
 	c |= 0x7f_f0_00_00_00_00_00_00 // set 11 bits on
 	c |= 0x00_08_00_00_00_00_00_00 // set 1 bit permanently
 
-	fmt.Printf("%064b\n", c)
-
-	fmt.Println("len is:", len(s))
-
-	c |= uint(len(s)) << 48
-
-	if len(s) > 6 {
-		return 0
-	}
+	c |= uint64(len(s)) << 48
 
 	for i := 0; i < len(s); i++ {
-		c |= uint(s[i]) << uint(8*i)
+		c |= uint64(s[i]) << (8 * i)
 	}
 
-	fmt.Printf("encoded: %#x\n", c)
+	return math.Float64frombits(c)
+}
 
-	fmt.Println("reverting")
+func decode(f float64) string {
+	fbs := math.Float64bits(f)
 
 	res := []byte{}
 
-	encodedlen := int(c & (0x07 << 48) >> 48)
+	encodedlen := int(fbs & (0x07 << 48) >> 48)
 
 	for i := 0; i < encodedlen; i++ {
-		// mask out particular byte
-		b := c & uint(0xff<<(8*i))
-
-		// shift it back
-		b >>= (8 * i)
-
-		fmt.Printf(">>>> %T %#[1]v\n", b)
+		// mask out particular byte and shift it back
+		b := fbs & uint64(0xff<<(8*i)) >> (8 * i)
 
 		res = append(res, byte(b))
 	}
 
-	fmt.Printf("reverted %s\n", string(res))
-
-	return c
+	return string(res)
 }
