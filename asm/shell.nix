ff6aaae01777207819056e8158ebfbd844fa56ce
Ilya Tribusean <slash3b@gmail.com>
2024-10-17 09:08:47 +0000

    misc

--- asm/shell.nix
+++ asm/shell.nix
@@ -1,7 +1,19 @@
-with (import <nixpkgs> {});
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
 
-mkShell {
-    buildInputs = [
-        nasm
-    ];
+pkgs.mkShell {
+  packages = with pkgs; [
+    cowsay
+    lolcat
+    nasm
+  ];
+
+    shellHook = ''
+        echo " nix-shell with nasm has started "
+        echo " ------------------------------- "
+        fish
+    '';
 }
+
