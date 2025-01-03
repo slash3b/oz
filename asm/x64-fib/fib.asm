8f1d1802a547c93a486a781c01537d5feb3ef07a
Ilya Tribusean <slash3b@gmail.com>
2025-01-03 14:30:17 +0200

    factorial done

--- asm/x64-fib/fib.asm
+++ asm/x64-fib/fib.asm
@@ -1,4 +1,113 @@
 section .text
 global fib
-fib:
-	ret
+global fac
+
+fac:
+    mov rax, rdi
+
+    cmp rdi, 0
+    je .end
+
+    call .factorial
+
+    ret
+
+.factorial:
+    cmp rax, 1
+    je .end
+
+    push rax
+    dec rax
+
+    call .factorial
+
+    ; ---------------------
+
+    pop rcx ; poping 
+
+    imul rax, rcx
+
+.end:
+    ret
+
+.end_rec:
+    pop rcx
+    imul rax, rcx
+    ret
+
+; fib:
+;     cmp rdi, 0
+;     je .end
+
+;     sub rdi, 1
+
+;     push rdi
+;     pop rdi
+
+;     add rdi, 10
+
+;     push rdi
+;     pop rax
+
+;     ret
+
+; .end:
+;     ret
+
+; .fib2:
+;     cmp rdi, 0
+;     je .end
+
+;     mov rax, 42
+
+;     dec rdi
+
+;     imul rax, rdi
+
+;     jmp .fib2
+
+; section .text
+; global fib
+
+; fib:
+;     cmp rdi, 2
+;     jl .short
+
+;     ; keep variables somewhere
+
+;     mov r8, 0
+;     mov r9, 1
+
+;     jmp .recfib
+
+;     ret
+
+; .fib2:
+;     cmp rdi, 0
+;     je .end
+
+;     dec rdi
+
+;     ; r10 is a temporary storage
+;     xor r10, r10
+;     mov r10, r8
+
+;     mov r8, r9
+;     mov r9, [r10 + r9]
+
+;     jmp .recfib
+
+; .short:
+;     mov rax, rdi
+
+;     ret
+
+; .end:
+;     xor rax, rax
+
+;     ; final answer
+
+;     add rax, r8
+;     add rax, r9
+
+;     ret
--- asm/x64-fib/tests.c
+++ asm/x64-fib/tests.c
@@ -2,10 +2,20 @@
 #include <stdio.h>
 
 extern int fib(int n);
+extern int fac(int n);
 
 int main(void) {
+  printf("fac(3) expected 6, got: %d\n", fac(3));
+  printf("fic(10) expected 3_628_800 %d\n", fac(10));
+  return 0;
+  printf("fib(3) expected 6 %d\n", fib(3));
+  return 0;
+
   assert(fib(0) == 0);
   assert(fib(1) == 1);
+  printf("fib(3) expected 2 %d\n", fib(3));
+  printf("fib(10) expected 55 %d\n", fib(10));
+  return 0;
   assert(fib(2) == 1);
   assert(fib(3) == 2);
   assert(fib(10) == 55);
